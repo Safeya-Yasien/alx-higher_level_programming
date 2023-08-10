80b53dc0e9dd495b88a792e9052fb04bf225adec
Safeya-Yasien <bluesky7yas@gmail.com>
2023-08-10 18:12:03 +0300

    how to make a script dynamic!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
+
+
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum = sum + int(sys.argv[i])
-    print(sum)
+    newsum = 0
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            newsum += int(argv[i])
+    print(newsum)
