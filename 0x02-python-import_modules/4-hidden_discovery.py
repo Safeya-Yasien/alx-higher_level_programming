dee337abd20741e73635549b63750c7fbb932249
Safeya-Yasien <bluesky7yas@gmail.com>
2023-08-10 18:10:29 +0300

    who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    name = dir(hidden_4)
-    for i in range(len(name)):
-        if name[i][0] != "_" and name[i][1] != "_":
-            print(name[i])
+    for a in dir(hidden_4):
+        if a[0] != "_" and a[1] != "_":
+            print("{}".format(a))
