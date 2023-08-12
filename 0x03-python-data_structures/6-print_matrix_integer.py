a05a37b1f8bae056d94431718b0cc31f097a7330
Safeya-Yasien <bluesky7yas@gmail.com>
2023-08-12 08:56:56 +0300

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j != 0:
-                print(" ", end="")
-            print("{:d}".format(matrix[i][j]), end="")
+    tam = len(matrix[0])
+    if tam is 0:
         print()
+    else:
+        tam = len(matrix[0])
+        cont = 0
+        for int in matrix:
+            for int2 in int:
+                cont += 1
+                if cont == tam:
+                    print("{:d}".format(int2), end="\n")
+                else:
+                    print("{:d} ".format(int2), end="")
+            cont = 0
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        list = [len(sentence), None]
+        new_tuplex = tuple(list)
+    else:
+        list = [len(sentence), sentence[0]]
+        new_tuplex = tuple(list)
+    return new_tuplex
