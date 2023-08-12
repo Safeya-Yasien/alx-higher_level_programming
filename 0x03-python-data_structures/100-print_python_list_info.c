aeb5ef1604c38dcc6d258720d3148d7bec5a0631
Safeya-Yasien <bluesky7yas@gmail.com>
2023-08-12 18:44:21 +0300

    advanced

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
+#include <Python.h>
+#include <stdio.h>
 
-/**
- *print_python_list_info - hola
- *@p: list
- *Return: number elements.
- */
-void print_python_list_info(PyObject *p)
-{
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size, i;
+    PyObject *item;
 
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %ld\n", size);
+
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+int main(void) {
+    PyObject *pList;
+    PyObject *pValue;
+
+    Py_Initialize();
+    pList = PyList_New(0);
+
+    pValue = PyLong_FromLong(1);
+    PyList_Append(pList, pValue);
+    Py_DECREF(pValue);
+
+    pValue = PyBytes_FromString("Python");
+    PyList_Append(pList, pValue);
+    Py_DECREF(pValue);
+
+    pValue = PyFloat_FromDouble(3.14);
+    PyList_Append(pList, pValue);
+    Py_DECREF(pValue);
+
+    print_python_list_info(pList);
+
+    Py_DECREF(pList);
+
+    return 0;
 }
