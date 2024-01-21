1511dfb158cf04af8e0b0549c6729f24d734d57f
Safeya-Yasien <bluesky7yas@gmail.com>
2024-01-21 15:11:50 +0200

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing']; // Use "JavaScript" with a capital "J"
 for (const i in words) {
   console.log(words[i]);
 }
