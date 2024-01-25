362e68b100b2e4bf4c255e3e88020b255eb54a6d
Safeya-Yasien <bluesky7yas@gmail.com>
2024-01-25 18:56:09 +0200

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# 1. cURL to the end
-curl -s -L --request GET "$1"
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""
-module
-"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """method
-    """
-    tmp_num = 0
-    if len(list_of_integers) > 0:
-        for iter in list_of_integers:
-            if iter > tmp_num:
-                tmp_num = iter
-        return(tmp_num)
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        pass
+        return find_peak(list_of_integers[mid + 1:])
