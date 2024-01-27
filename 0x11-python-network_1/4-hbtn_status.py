32aa28b753d69b32d24f4fab2ad38c467607e185
Safeya-Yasien <bluesky7yas@gmail.com>
2024-01-27 07:43:11 +0200

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests"""
+"""fetches http://0.0.0.0:5050/status using requests"""
 import requests
 
-
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
+    """fetches http://0.0.0.0:5050/status using requests"""
+    r = requests.get('http://0.0.0.0:5050/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
